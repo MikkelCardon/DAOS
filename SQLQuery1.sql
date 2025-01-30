8353ea12de3612cb78ef23471fc88389535a9462
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-01-30 16:22:54 +0100

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,18 +1,3 @@
-DROP TABLE DataEks;
-
-CREATE TABLE DataEks(
-id INT PRIMARY KEY,
-kommatal DECIMAL(6,2),
-navn VARCHAR(20),
-dato DATE DEFAULT getDate(),
-kort char(8) CHECK(KORT IN('klør', 'spar', 'ruder', 'hjerter'))
-)
-
-
-INSERT INTO DataEks(id, kommatal, navn, kort) VALUES
-(4, 4000.5, 'Ole', 'spar')
-
-SELECT * FROM DataEks
 
 
 
